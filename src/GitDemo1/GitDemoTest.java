322c1f2e6cdb978aad014c2b2cc54a85f0b9beaf
tapak <tapak@DESKTOP-1K6GH5S>
2022-10-19 17:14:36 -0500

    First commit

---
+++ src/GitDemo1/GitDemoTest.java
@@ -0,0 +1,17 @@
+package GitDemo1;
+
+public class GitDemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes");
+	
+	System.out.println(" Eat something");
+		
+		} 
+	
+			
+		}
---
+++ src/GitDemo1/package-info.java
@@ -0,0 +1,1 @@
+package GitDemo1;
